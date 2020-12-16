1642fc890ebb3c439f276666eb2bbfed9c33e614
samharshe <samharshe29@gmail.com>
2020-12-16 12:28:22 -0500

    made slider label update with its value

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-// var aggresiveness_value = document.getElementById("leftLabel");
-// console.log(aggresiveness_value);
+var aggressiveness_slider = document.getElementById("aggressiveness");
+aggressiveness_slider.value = 0;
 
-console.log("HELLO!");
+function updateLabelValue(){
+    var aggressiveness_value = aggressiveness_slider.value * 10 + 10;
+    console.log("aggres: " + aggressiveness_value);
+
+    var current_value_label = document.getElementById("currentValueLabel");
+
+    current_value_label.innerHTML = aggressiveness_value;
+};
